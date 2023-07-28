63fe4a1725aa20d8d99663d8c37d9c4a44144376
Samuel Henrique <samueel.jpg21@gmail.com>
2023-07-28 13:54:09 -0300

    explicando como a função funciona

--- funcao.js
+++ funcao.js
@@ -1,14 +1,6 @@
-function getFlag(string){
-   const index = process.argv.indexOf(string) + 1;
+function getFlag(flag){
+   const index = process.argv.indexOf(flag) + 1;
    return process.argv[index];
 }
 module.exports = getFlag;
 
-********* SABER COMO FUNCIONA O CINEMA DEPOIS QUE O SHOPPING FECHA
-crie 2 arquivos javascript
-o primeiro, irá exportar uma função chamada getFlag() que receberá um argumento do tipo string. Essa função deverá buscar
-dentro do array process.argv a flag deseja, que é a String e retornar o valor da flag
-
-O segundo irá importar a função e passar a flag desejada
-
-Iremos rodar no terminal o segundo arquivo as flags --name e --greeting para que seja impresso no terminal a saudação e o nome da pessoa.
--- importandoFuncao.js
+++ importandoFuncao.js
@@ -1,2 +1,13 @@
+i/* importando função do arquivo funcao.js */
 const getFlag = require('./funcao');
+/* quando imprimo isso a função é executada passando a string --name como argumento pra função */
 console.log(`Olá ${getFlag('--name')}`)
+
+
+/* ENTAO A FUNÇÃO É EXECUTADA E DENTRO DA CONSTANTE index É SALVO O ÍNDICE + 1 ONDE O ARGUMENTO FOI PASSADO  */
+/* function getFlag(flag){
+   const index = process.argv.indexOf(flag) + 1;
+   DAI A FUNÇÃO RETORNA O ELEMENTO DO ARRAY PROCESS.ARGV ONDE O ÍNDICE FOI PASSADO 
+   return process.argv[index];
+}
+module.exports = getFlag; */
