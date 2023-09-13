a10d23ac92e26ff567bedb15ddddb8fc2b64f24d
Samuel Henrique <samueel.jpg21@gmail.com>
2023-09-13 16:56:03 -0300

    .

--- teste.js
+++ teste.js
@@ -2,7 +2,7 @@ function showMenu(){
     let navLinks = document.querySelector('.navigation-links-mobile');
     let verLinks = navLinks.classList.contains('open');
     let iconMenu = document.querySelector('.menu');
-    let navAfter = 
+
     if(verLinks){
         navLinks.classList.remove('open');
         iconMenu.src = "/assets/close_white_36dp.svg"
